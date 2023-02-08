3de29ad65357ea7a98043a0617baedad1c731f54
御風行 <1587898817@qq.com>
2023-02-08 13:48:36 -0500

    initial commit

---
+++ src/main/java/com/singfung/acl/AclTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.singfung.acl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AclTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AclTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/singfung/acl/AclTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singfung.acl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AclTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
