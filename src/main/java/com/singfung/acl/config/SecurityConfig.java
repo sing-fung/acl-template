fcff32f1238e9187c009654c29b387ed24691fcd
御風行 <1587898817@qq.com>
2023-02-09 16:04:30 -0500

    disable Spring Security

---
+++ src/main/java/com/singfung/acl/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.singfung.acl.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .antMatchers("/**");
+    }
+}
