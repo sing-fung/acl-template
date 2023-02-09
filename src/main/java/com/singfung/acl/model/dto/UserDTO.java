9f82c5599f5a8d081e995c8805dfb515c0ad7e21
sing-fung <1587898817@qq.com>
2023-02-08 23:38:56 -0500

    change validation dependency

--- src/main/java/com/singfung/acl/model/dto/UserDTO.java
+++ src/main/java/com/singfung/acl/model/dto/UserDTO.java
@@ -1,7 +1,8 @@
 package com.singfung.acl.model.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class UserDTO
