9648e3f6b75e0ef7ee4e16359aa3bb3faf71dcc3
御風行 <1587898817@qq.com>
2023-02-09 13:40:59 -0500

    EncoderConfig

---
+++ src/main/java/com/singfung/acl/config/EncoderConfig.java
@@ -0,0 +1,14 @@
+package com.singfung.acl.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class EncoderConfig
+{
+    @Bean
+    public BCryptPasswordEncoder createEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
